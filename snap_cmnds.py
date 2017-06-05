6ff9646e47b7c4eea1878275a6bf1a3ed4f2c28e
Court Campbell <cwcampbell@eprod.com>
2017-06-05 07:02:52 -0500

    Updated snap_cmnds.py

--- snap_cmnds.py
+++ snap_cmnds.py
@@ -13,33 +13,33 @@ def check_sid(sid):
 
 def check_snap_sg(dest_sid):
   sg_name = ''.join(host + "_" + dest_sid + "_snap").upper()
-  print "Checking for SG " + sg_name
+  print "Checking for SG", sg_name
   symsg_show = subprocess.Popen(['symsg', '-sid', array_id, 'show', sg_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   symsg_show.wait()
   if symsg_show.returncode != 0:
-    print " * SG does not exist. Creating SG " + sg_name
+    print " * SG does not exist. Creating SG", sg_name
     symsg_create = subprocess.Popen(['symsg', '-sid', array_id, 'create', sg_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     symsg_create.wait()
     if symsg_create.returncode != 0:
       print " * Error creating SG. Exiting."
       exit(1)
   else:
-    print " * SG " + sg_name + " exists. Continuing..."
+    print " * SG", sg_name, "exists. Continuing..."
 
 
 def check_snap_tdev(src_sid,dest_sid):
   tdev_name = ''.join(host + "_" + dest_sid + "_snap").upper()
   sg_name = tdev_name
-  print "Checking if tdev " + tdev_name + " exists."
+  print "Checking if tdev", tdev_name, "exists."
   symdev_list = subprocess.Popen(['symdev', '-sid', array_id, 'list', '-identifier', 'device_name'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   grep_symdev_list = subprocess.Popen(['grep', '-w', tdev_name], stdin=symdev_list.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   grep_symdev_list.wait()
   if grep_symdev_list.returncode != 0:
-    print " * Target device does not exist. Creating device " + tdev_name + "."
+    print " * Target device does not exist. Creating device", tdev_name, "."
     src_dev_size = get_source_disk_size(src_sid)
     symconf_status = subprocess.Popen(['symconfigure', '-sid', array_id, '-cmd', "create dev count=1,size=" + src_dev_size + "mb,config=tdev,emulation=fba,preallocate size=all,sg=" + sg_name + ",device_name=" + tdev_name, 'commit', '-nop'])
   else:
-     print " * Device " + tdev_name + "exists. Continuing..."
+     print " * Device", tdev_name, "exists. Continuing..."
     
 
 def check_snap_tdev_size(src_sid,dest_sid):
@@ -49,16 +49,16 @@ def check_snap_tdev_size(src_sid,dest_sid):
   src_dev_size = get_source_disk_size(src_sid)
   dest_dev_size = get_target_disk_size(dest_sid)
   if src_dev_size > dest_dev_size: 
-    print " * Source device is larger than destination device. Attempting to grow target device " + tdev_name + "."
-    print " * Getting symid for " + tdev_name + "."
+    print " * Source device is larger than destination device. Attempting to grow target device ", tdev_name, "."
+    print " * Getting symid for ", tdev_name, "."
     symdev_list = subprocess.Popen(['symdev', '-sid', array_id, 'list', '-identifier', 'device_name'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     grep_symdev_list = subprocess.Popen(['grep', '-w', tdev_name], stdin=symdev_list.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     tdev_num = grep_symdev_list.communicate()[0].split()[0]
-    print " * Target device number is " + tdev_num + "."
+    print " * Target device number is ", tdev_num, "."
     symdev_resize = subprocess.Popen(['symdev', '-sid', array_id, 'modify', tdev_num, '-tdev', '-cap', src_dev_size, '-captype', 'MB', '-nop'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     symdev_resize.wait()
     if symdev_resize.returncode != 0:
-      print " * Error growing target device " + tdev_name + ". Exiting."
+      print " * Error growing target device ", tdev_name, ". Exiting."
       exit(1)
   else: 
     print " * Target device size matches source. Continuing..."
@@ -66,36 +66,36 @@ def check_snap_tdev_size(src_sid,dest_sid):
 
 def get_source_disk_size(src_sid):
   src_dev_name = ''.join(sids[src_sid] + "_" + src_sid).upper()
-  print " * Getting source device size for " + src_dev_name + "."
+  print " * Getting source device size for ", src_dev_name, "."
   symdev_list = subprocess.Popen(['symdev', '-sid', array_id, 'list', '-identifier', 'device_name'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   grep_symdev_list = subprocess.Popen(['grep', '-w', src_dev_name], stdin=symdev_list.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   dev_num = grep_symdev_list.communicate()[0].split()[0].strip()
-  print "   * Sym device name is " + dev_num + "."
+  print "   * Sym device name is ", dev_num, "."
   src_device_info = subprocess.Popen(['symdev', '-sid', array_id, 'list', '-devs', dev_num], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   grep_src_device_info = subprocess.Popen(['grep', '-w', dev_num], stdin=src_device_info.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   src_dev_size = grep_src_device_info.communicate()[0].split()[-1].strip()
-  print "   * Sym device size is " + src_dev_size + "MB."
+  print "   * Sym device size is ", src_dev_size, "MB."
   return src_dev_size
 
 
 def get_target_disk_size(dest_sid):
   dest_tdev_name = ''.join(host + "_" + dest_sid + "_snap").upper()
-  print " * Getting target device size for " + dest_tdev_name + "."
+  print " * Getting target device size for ", dest_tdev_name, "."
   symdev_list = subprocess.Popen(['symdev', '-sid', array_id, 'list', '-identifier', 'device_name'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   grep_symdev_list = subprocess.Popen(['grep', '-w', dest_tdev_name], stdin=symdev_list.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   dev_num = grep_symdev_list.communicate()[0].split()[0].strip()
-  print "   * Sym device name is " + dev_num + "."
+  print "   * Sym device name is ", dev_num, "."
   src_device_info = subprocess.Popen(['symdev', '-sid', array_id, 'list', '-devs', dev_num], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   grep_dest_device_info = subprocess.Popen(['grep', '-w', dev_num], stdin=src_device_info.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   dest_dev_size = grep_dest_device_info.communicate()[0].split()[-1].strip()
-  print "   * Sym device size is " + dest_dev_size + "MB."
+  print "   * Sym device size is ", dest_dev_size, "MB."
   return dest_dev_size
 
 
 def create_snap(src_sid,dest_sid):
   snap_name = ''.join(host + "_" + dest_sid + "_snap").upper()
   src_sg_name = ''.join(sids[src_sid] + "_" + src_sid).upper()
-  print "Checking if snapshot " + src_sg_name + " exists."
+  print "Checking if snapshot ", src_sg_name, " exists."
   snapvx_status = subprocess.Popen(['symsnapvx', '-sid', array_id, 'list', '-sg', src_sg_name, '-snapshot_name', snap_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   snapvx_status.wait()
   if snapvx_status.returncode != 0: 
@@ -103,11 +103,11 @@ def create_snap(src_sid,dest_sid):
     snapvx_establish = subprocess.Popen(['symsnapvx', '-sid', array_id, '-sg', src_sg_name, '-name', snap_name, 'establish', '-nop'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     snapvx_establish.wait()
     if snapvx_establish.returncode != 0:
-      print " * Error creating snap " + snap_name + " from " + src_sg_name + ". Exiting."
+      print " * Error creating snap ", snap_name, " from ", src_sg_name, ". Exiting."
       exit(1)
     symsnapvx_list = subprocess.Popen(['symsnapvx', '-sid', array_id, 'list', '-sg', src_sg_name, '-snapshot_name', snap_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   else:
-    print " * Snap " + snap_name + " exists. Continuing..."
+    print " * Snap ", snap_name, " exists. Continuing..."
 
 
 def link_snap(src_sid,dest_sid):
@@ -118,21 +118,21 @@ def link_snap(src_sid,dest_sid):
   snapvx_link_status = subprocess.Popen(['symsnapvx', '-sid', array_id, 'list', '-sg', src_sg_name, '-snapshot_name', snap_name, '-linked'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   snapvx_link_status_out, snapvx_link_status_err = snapvx_link_status.communicate() 
   if "do not have any Snapvx information" in snapvx_link_status_err:
-    print " * Snap is not linked to an SG. Creating link of " + snap_name + " to " + dest_sg_name + "."
+    print " * Snap is not linked to an SG. Creating link of ", snap_name, " to ", dest_sg_name, "."
     snapvx_link = subprocess.Popen(['symsnapvx', '-sid', array_id, '-sg', src_sg_name, '-lnsg', dest_sg_name, '-snapshot_name', snap_name, 'link', '-copy', '-nop'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     snapvx_link.wait() 
     if snapvx_link.returncode != 0:
-      print " * Error linking snap " + snap_name + " to " + dest_sg_name + ". Exiting."
+      print " * Error linking snap ", snap_name, " to ", dest_sg_name, ". Exiting."
       exit(1)
   else: 
-    print " * Link of " + snap_name + " to " + dest_sg_name + " already exists. Continuing..."
+    print " * Link of ", snap_name, " to ", dest_sg_name, " already exists. Continuing..."
     
 
 def unlink_and_terminate_snap(src_sid,dest_sid):
   snap_name = ''.join(host + "_" + dest_sid + "_snap").upper()
   src_sg_name = ''.join(sids[src_sid] + "_" + src_sid).upper()
   dest_sg_name = "{dest_host}_{dest_sid}_snap".format(dest_host=host,dest_sid=dest_sid).upper()
-  print "Unlinking snap {snap_name} from {src_sg_name}.".format(snap_name=snap_name,src_sg_name=src_sg_name)
+  print "Unlinking snap", snap_name, "from", src_sg_name, "."
   snapvx_unlink = subprocess.Popen(['symsnapvx', '-sid', array_id, '-sg', src_sg_name, '-lnsg', dest_sg_name, '-snapshot_name', snap_name, 'unlink', '-symforce', '-nop'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   snapvx_unlink_out,snapvx_unlink_err =  snapvx_unlink.communicate()
   if snapvx_unlink.returncode != 0:
@@ -143,7 +143,7 @@ def unlink_and_terminate_snap(src_sid,dest_sid):
       exit(1)
   else:
     print " * Unlink successful!"
-  print "Terminating snap {snap_name}.".format(snap_name=snap_name)
+  print "Terminating snap", snap_name, "."
   snapvx_terminate = subprocess.Popen(['symsnapvx', '-sid', array_id, '-sg', src_sg_name, '-snapshot_name', snap_name, 'terminate', '-nop'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   snapvx_terminate_out, snapvx_terminate_err = snapvx_terminate.communicate()
   if snapvx_terminate.returncode != 0:
@@ -159,31 +159,31 @@ def unlink_and_terminate_snap(src_sid,dest_sid):
 def add_sg_to_mv(dest_sid):
   dest_sg_name = "{dest_host}_{dest_sid}_SNAP".format(dest_host=host,dest_sid=dest_sid).upper()
   parent_sg_name = "{dest_host}_SG".format(dest_host=host).upper()
-  print "Checking if {dest_sg_name} is already a child of {parent_sg_name}.".format(dest_sg_name=dest_sg_name,parent_sg_name=parent_sg_name)
+  print "Checking if", dest_sg_name, "is already a child of", parent_sg_name, "."
   symsg_parent_info = subprocess.Popen(['symsg', '-sid', array_id, 'show', parent_sg_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   grep_symsg_parent_info = subprocess.Popen(['grep', '-w', dest_sg_name], stdin=symsg_parent_info.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   if "IsChild" in grep_symsg_parent_info.communicate()[0]: 
-    print " * {dest_sg_name} is already a child of SG {parent_sg_name}. Continuing...".format(dest_sg_name=dest_sg_name,parent_sg_name=parent_sg_name)
+    print " *", dest_sg_name, "is already a child of SG", parent_sg_name," . Continuing..."
   else: 
-    print " * {dest_sg_name} is not a child of {parent_sg_name}. Adding.".format(dest_sg_name=dest_sg_name,parent_sg_name=parent_sg_name)
+    print " *", dest_sg_name, "is not a child of", parent_sg_name, ". Adding."
     symsg_add_status = subprocess.Popen(['symsg', '-sid', array_id, '-sg', parent_sg_name, 'add', 'sg', dest_sg_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     symsg_add_status.wait()
     if symsg_add_status.returncode != 0: 
-      print " * Error adding {dest_sg_name} to parent_sg_name. Exiting.".format(dest_sg_name=dest_sg_name,parent_sg_name=parent_sg_name)
+      print " * Error adding", dest_sg_name, "to", parent_sg_name, ". Exiting."
       exit(1)
         
     
 def remove_sg_from_mv(dest_sid):
   sg_name = "{host}_{dest_sid}_SNAP".format(host=host,dest_sid=dest_sid).upper()
   parent_sg_name = "{dest_host}_SG".format(dest_host=host).upper()
-  print "Removing {sg_name} from parent {parent_sg_name}.".format(sg_name=sg_name,parent_sg_name=parent_sg_name)
+  print "Removing", sg_name, "from parent", parent_sg_name, "."
   symsg_remove = subprocess.Popen(['symsg', '-sid', array_id, '-sg', parent_sg_name, 'remove', 'sg', sg_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   symsg_remove_out,symsg_remove_err = symsg_remove.communicate()
   if symsg_remove.returncode != 0:
     if "No Storage groups" in symsg_remove_err.strip():
       print " * Storage group not found in parent. Continuing..."
     else:
-      print " * Error removing {sg_name} from {parent_sg_name}. Exiting.".format(sg_name=sg_name,parent_sg_name=parent_sg_name)
+      print " * Error removing", sg_name, "from", parent_sg_name, ". Exiting."
       exit(1)
    
 
@@ -204,7 +204,7 @@ def disk_cleanup(dest_sid):
   if "Not found" in disk_dev_file:
     print " * No disk found. Continuing..."
   else: 
-    print "Removing disk {disk_dev_file}.".format(disk_dev_file=disk_dev_file)
+    print "Removing disk", disk_dev_file, "."
     remove_disk_status = subprocess.Popen(['/etc/opt/emcpower/emcplun_linux', 'remove', disk_dev_file, '-noprompt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     remove_disk_status.wait()
     if remove_disk_status.returncode != 0: 
@@ -217,7 +217,7 @@ def import_activate_vg(dest_sid):
   vg_basename = "vg" + dest_sid
   vgs_returncode = check_vgs(vg_basename)
   if vgs_returncode == 0:
-    print " * Volume group {vg} is already imported. Continuing...".format(vg=vg_basename) 
+    print " * Volume group", vg_basenamei, "is already imported. Continuing..."
   else:
     print "Importing and Activating volume group"
     print " * Running vgimportclone with basename vg{dest_sid}.".format(dest_sid=dest_sid)
